b996c963919f01f33ead4fb257c7f21e93029e9e
daoswap002 <daoswap@daoswap.cc>
2023-02-20 14:46:53 +0800

    modify truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const mnemonic = fs.readFileSync(".mnemonic").toString().trim();
+const mnemonicTestnet = fs.readFileSync("../mnemonic/.mnemonic-testnet").toString().trim();
+const mnemonicMainnet = fs.readFileSync("../mnemonic/.mnemonic-mainnet").toString().trim();
+const apiKeysBscscan = fs.readFileSync("../mnemonic/.api_keys_bscscan").toString().trim();
 
 module.exports = {
   networks: {
     hecotestnet: {
-      provider: () => new HDWalletProvider(mnemonic, 'https://http-testnet.hecochain.com'),
+      provider: () => new HDWalletProvider(mnemonicTestnet, 'https://http-testnet.hecochain.com'),
       network_id: 256,
       // from: '0xa9bB710996d6ed61B83a5EAB583bAe683199c2de',        // Account to send txs from (default: accounts[0])
       // gas: 550000,        // Ropsten has a lower block limit than mainnet
@@ -16,7 +18,7 @@ module.exports = {
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
     hecomainnet: {
-      provider: () => new HDWalletProvider(mnemonic, 'https://http-mainnet.hecochain.com'),
+      provider: () => new HDWalletProvider(mnemonicMainnet, 'https://http-mainnet.hecochain.com'),
       network_id: 128,
       // from: '0xa9bB710996d6ed61B83a5EAB583bAe683199c2de',        // Account to send txs from (default: accounts[0])
       // gas: 5500000,        // Ropsten has a lower block limit than mainnet
@@ -26,14 +28,16 @@ module.exports = {
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
     bsctestnet: {
-      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      provider: () => new HDWalletProvider(mnemonicTestnet, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
       // confirmations: 10,
       // timeoutBlocks: 200,
       // skipDryRun: true
     },
     bscmainnet: {
-      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      // provider: () => new HDWalletProvider(mnemonicMainnet, `https://bsc-dataseed1.binance.org`),
+      // provider: () => new HDWalletProvider(mnemonicMainnet, `https://bsc-dataseed1.ninicoin.io`),
+      provider: () => new HDWalletProvider(mnemonicMainnet, `https://bsc-dataseed2.defibit.io`),
       network_id: 56,
       // confirmations: 10,
       // timeoutBlocks: 200,
@@ -44,6 +48,12 @@ module.exports = {
   // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000
+  },
+
+  plugins: ['truffle-plugin-verify'],
+  api_keys: {
+    bscscan: apiKeysBscscan,
+    hecoinfo: ''
   },
 
   // Configure your compilers
